fe5463b69e32c4cd7affae30812e053148533e7a
rajankouzina <61734099+rajankouzina@users.noreply.github.com>
2021-07-10 05:00:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ server.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Content-Range', 'posts 0-20/20')
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
+    res.setHeader('Content-Range', 'posts 0-24/319');
     next();
 });
 server.use(router);

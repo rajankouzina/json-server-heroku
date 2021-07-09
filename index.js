1452505e3140c4e636628f9bde06c83fa3f4402b
rajankouzina <61734099+rajankouzina@users.noreply.github.com>
2021-07-10 04:40:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+
+
 server.use(middlewares);
+server.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Content-Range', 'posts 0-20/20')
+    next();
+});
 server.use(router);
 
 server.listen(port);

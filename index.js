f78a5207e5509d7b8baaddd520e8c42b82a73da0
rajankouzina <61734099+rajankouzina@users.noreply.github.com>
2021-08-03 18:32:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
 
@@ -10,7 +10,7 @@ server.use(middlewares);
 server.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
     res.setHeader('Content-Range', 'posts 0-24/319');

3dc41ba5e594a41d004090d14006e424feaaaae7
rajankouzina <61734099+rajankouzina@users.noreply.github.com>
2021-08-03 18:35:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const port = process.env.PORT || 8080;
 server.use(middlewares);
 server.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
-    res.setHeader('Content-Range', 'posts 0-24/319');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+    // res.setHeader('Access-Control-Expose-Headers', 'Content-Range');
+    // res.setHeader('Content-Range', 'posts 0-24/319');
     next();
 });
 server.use(router);
